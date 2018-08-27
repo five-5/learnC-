6fc09b743a34dbe08b9d749c1e811127d02c2f8b
shenhuan <shenhuan1996@qq.com>
2018-08-27 11:12:35 +0800

    test for exercise 3.11

---
+++ C++ primer/cha3/3.11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    const string s = "Keep out!";
+    for (auto &c : s){
+       /**/
+    }
+    cout << s;
+    return 0;
+}
