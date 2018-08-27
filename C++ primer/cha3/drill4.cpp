5cb5fd2b0a09f3366ebf3d0f11ae4d8e21f18bad
shenhuan <shenhuan1996@qq.com>
2018-08-27 10:47:51 +0800

    add random access char in string -- transfer decimal to hex

--- C++ primer/cha3/drill4.cpp
+++ C++ primer/cha3/drill4.cpp
@@ -3,6 +3,8 @@
 #include <cctype>
 using namespace std;
 
+char toHex(int number);
+
 int main()
 {
     string s("some string");
@@ -28,5 +30,53 @@ int main()
          s[index] = toupper(s[index]);
     cout << s << endl;
 
+    // 把0到15之间的十进制数转换成对应的十六进制形式
+    // 个人版
+    int n;
+    string result;
+    cout << "Enter a series of numbers between 0 and 15"
+         << " separated by spaces. Hit ENTER when finished: "
+         << endl;
+    while (cin >> n){
+        if (n >= 0 && n <= 15)
+        {
+            result += toHex(n);
+        }
+    }
+    cout << "Your hex number is: " << result << endl;
+
+    // 书本版
+    const string hexdigits = "0123456789ABCDEF";
+    cout << "Enter a series of numbers between 0 and 15"
+         << " separated by spaces. Hit ENTER when finished: "
+         << endl;
+    result = "";
+    string::size_type nu;
+    while (cin >> nu)
+        if (nu < hexdigits.size())
+            result += hexdigits[nu];
+    cout << "Your hex number is: " << result << endl;
     return 0;
 }
+
+char toHex(int number)
+{
+    if (number <= 9)
+        return '0' + number;
+    else{
+        switch(number)
+        {
+            case 10: return 'A';
+            case 11: return 'B';
+            case 12: return 'C';
+            case 13: return 'D';
+            case 14: return 'E';
+            case 15: return 'F';
+        }
+    }
+}
+
+/*
+    固定的转换输出可以设置常量数组
+    确定不会小于0的数字设置为无符号数
+*/
