11baf54bfb7295d524abeed127b3675499b1d427
shenhuan <shenhuan1996@qq.com>
2018-08-28 11:16:31 +0800

    program for exercise 4.5

---
+++ C++ primer/cha4/4.5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "(a) " << -30 * 3 + 21 / 5 << endl;
+    cout << "(b) " << -30 + 3 * 21 / 5 << endl;
+    cout << "(c) " << -30 / 3 * 21 % 5 << endl;
+    cout << "(d) " << -30 / 3 * 21 % 4 << endl;
+    return 0;
+}
