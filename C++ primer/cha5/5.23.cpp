0a397ed85dc0297b81f21b495bf43b5ea60d961a
shenhuan <shenhuan1996@qq.com>
2018-08-29 11:15:06 +0800

    program for exercise 5.23

---
+++ C++ primer/cha5/5.23.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1, var2;
+    cout << "Please enter two integers: ";
+    cin >> var1 >> var2;
+    cout << var1 / var2 << endl;
+
+    return 0;
+}
