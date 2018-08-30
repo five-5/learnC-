4db8ebd4f0704028870a5b5a368fc3007030fb86
shenhuan <shenhuan1996@qq.com>
2018-08-30 19:05:30 +0800

    add friend

--- C++ primer/cha7/ex7_12.h
+++ C++ primer/cha7/ex7_12.h
@@ -8,20 +8,26 @@ struct Sales_data;
 std::istream &read(std::istream&, Sales_data&);
 
 struct Sales_data{
-    // constructor
-    Sales_data() = default;
-    Sales_data(const std::string &s) : bookNo(s){}
-    Sales_data(const std::string &s, unsigned n, double p) :
-               bookNo(s), units_sold(n), revenue(p*n){}
-    Sales_data(std::istream &is){ read(is, *this);}
+	public:
+		// constructor
+		Sales_data() = default;
+		Sales_data(const std::string &s) : bookNo(s){}
+		Sales_data(const std::string &s, unsigned n, double p) :
+				   bookNo(s), units_sold(n), revenue(p*n){}
+		Sales_data(std::istream &is){ read(is, *this);}
 
-    // before
-    std::string const& isbn() const{ return bookNo;}
-    Sales_data& combine(const Sales_data&);
+		// before
+		std::string const& isbn() const{ return bookNo;}
+		Sales_data& combine(const Sales_data&);
 
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
+	private:
+		std::string bookNo;
+		unsigned units_sold = 0;
+		double revenue = 0.0;
+	
+	friend std::istream &read(std::istream &is, Sales_data &item);
+	friend std::ostream &print(std::ostream &os, const Sales_data &item);
+	friend Sales_data add(const Sales_data &lhs, const Sales_data &rhs);
 };
 
 // member function
