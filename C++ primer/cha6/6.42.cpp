5b0afc3c07d8fd4e1e8bf6d1e147b6ddeb17393b
shenhuan <shenhuan1996@qq.com>
2018-08-30 09:05:23 +0800

    program for exercise 6.42

---
+++ C++ primer/cha6/6.42.cpp
@@ -0,0 +1,19 @@
+// 给make_plural函数的第二个形参赋予默认参数's'，利用新版本的函数输出单词success和failure的单数和复数形式。
+#include <iostream>
+#include <string>
+using namespace std;
+
+string make_plural(size_t ctr, const string &word, const string &ending = "s")
+{
+    return (ctr > 1)? word + ending : word;
+}
+
+int main()
+{
+    cout << "singular: " << make_plural(1, "success", "es") << " "
+         << make_plural(1, "failure") << endl;
+    cout << "plural: " << make_plural(2, "success", "es") << " "
+         << make_plural(2, "failure") << endl;
+
+    return 0;
+}
