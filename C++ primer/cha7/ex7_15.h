295ef57229820a57148a3ecfbfd27c54fea6b939
shenhuan <shenhuan1996@qq.com>
2018-08-30 17:20:58 +0800

    practise for exercise 7.15

---
+++ C++ primer/cha7/ex7_15.h
@@ -0,0 +1,30 @@
+#ifndef EX7_15_H_INCLUDED
+#define EX7_15_H_INCLUDED
+
+#include <string>
+#include <iostream>
+
+class Person{
+    // constructor
+    Person(const string &Name, const string &Addr):name(Name), addr(Addr){}
+
+    string const& getName() const{ return name;};
+    string const& getAddr() const{ return addr;};
+    std::string name;
+    std::string addr;
+};
+
+// non-member functions
+std::istream& read(std::istream &is, Person &person)
+{
+    is >> person.name >> person.addr;
+    return is;
+}
+
+std::ostream& print(std::ostream &os, const Person &person)
+{
+    os << person.getName() << " " << person.getAddr();
+    return os;
+}
+
+#endif // EX7_15_H_INCLUDED
