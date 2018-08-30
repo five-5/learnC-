ffdd8108e68a13dc02b6f5247d7085d0f900dd47
shenhuan <shenhuan1996@qq.com>
2018-08-30 20:12:56 +0800

    add display function

--- C++ primer/cha7/Screen.h
+++ C++ primer/cha7/Screen.h
@@ -1,19 +1,50 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <string>
+#include <iostream>
+
 class Screen{
 	public:
 		typedef std::string::size_type pos;
+
 		Screen() = default;
+		Screen(pos ht, pos wd) : height(ht), width(wd), contents(ht*wd, ' '){}
 		Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht*wd, c){}
+
 		char get() const{ return contents[cursor];}
 		inline char get(pos ht, pos wd) const;
+
 		Screen &move(pos r, pos c);
-		void some_member() const;
-		
+		Screen &set(char);
+		Screen &set(pos, pos, char);
+
+		Screen &display(std::ostream &os) { do_display(os); return *this; }
+		const Screen &display(std::ostream &os) const { do_display(os); return *this; }
+
+    private:
+        void do_display(std::ostream &os) const { os << contents; }
+
 	private:
-		mutable size_t access_ctr;
 		pos cursor = 0;
 		pos height = 0, width = 0;
 		std::string contents;
+
 };
+
+inline
+Screen &Screen::set(char c)
+{
+	contents[cursor] = c;
+	return *this;
+}
+
+inline
+Screen &Screen::set(pos r, pos col, char ch)
+{
+	contents[r * width + col] = ch;
+	return *this;
+}
 
 inline
 Screen &Screen::move(pos r, pos c)
@@ -29,8 +60,4 @@ char Screen::get(pos r, pos c) const
 	return contents[row + c];
 }
 
-void Screen::some_member() const
-{
-	++access_ctr;          // save the called times
-	// todo
-}
+#endif
