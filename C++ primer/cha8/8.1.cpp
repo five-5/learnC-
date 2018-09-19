754acf88fe0b2c4a2206ac6408739a7cb6b0c2f8
shenhuan <shenhuan1996@qq.com>
2018-09-19 10:47:44 +0800

    practice for exercise 8.1

---
+++ C++ primer/cha8/8.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+istream& func(istream &in)
+{
+    // 从给定流中读取数据
+    string buf;
+    while(in >> buf)
+        // 读取的数据打印在标准输出上
+        cout << buf << endl;
+    // 对流进行复位
+    in.clear();
+    return in;
+}
+
+int main()
+{
+    return 0;
+}
