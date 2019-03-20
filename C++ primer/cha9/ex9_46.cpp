6c4d552f6e47d6348f8a535127c324b1a7af2cf9
five-5 <shenhuan1996@qq.com>
2019-03-20 17:50:12 +0800

    ex9.46

---
+++ C++ primer/cha9/ex9_46.cpp
@@ -0,0 +1,26 @@
+// Copyright 2019 @five-5
+// License(Apache License)
+// Author: five-5
+// This is just for cpp primer ex9.46 :
+// accept strname and prefix and suffix use insert postion and lenth info to add to name
+
+#include <iostream>
+#include <string>
+
+std::string add_pre_and_suf(
+    const std::string &str_name, 
+    const std::string &prefix, 
+    const std::string &suffix) {
+        std::string str_temp = str_name;
+        str_temp.insert(str_temp.begin(), prefix.begin(), prefix.end());
+        str_temp.insert(str_temp.end(), suffix.begin(), suffix.end());
+        return str_temp;
+}
+
+int main()
+{
+    std::string str_name = "five-5", prefix = "Ms.", suffix = "III";
+    std::cout << add_pre_and_suf(str_name, prefix, suffix);
+    
+    return 0;
+}
