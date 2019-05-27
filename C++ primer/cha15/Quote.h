4924167203283d026003aebe9bc4b6077f999b0f
five-5 <shenhuan1996@qq.com>
2019-05-27 23:12:32 +0800

    ex15_26

--- C++ primer/cha15/Quote.h
+++ C++ primer/cha15/Quote.h
@@ -2,7 +2,7 @@
  * @Author: five-5
  * @Date: 2019-05-22
  * @Description: definetion of quote and bulk_quote
- * @LastEditTime: 2019-05-23
+ * @LastEditTime: 2019-05-27
  */
 
 #ifndef QUOTE_H
@@ -12,10 +12,47 @@
 #include <iostream>
 
 class Quote{
+    friend bool operator !=(const Quote& lhs, const Quote& rhs)
+    {
+        return lhs.bookNo != rhs.bookNo && lhs.price != rhs.price;
+    }
  public:
-    Quote() = default;
+    Quote() { std::cout << "default constructing Quote" << std::endl; }
     Quote(const std::string &book, double sales_price) :
-            bookNo(book), price(sales_price) {}
+            bookNo(book), price(sales_price) { std::cout << "Quote: constructor takes 2 para.\n"; }
+
+    // copy constructor
+    Quote(const Quote& q) : bookNo(q.bookNo), price(q.price)
+    { std::cout << "Quote: copy constructor\n"; }
+
+    // move constructor
+    Quote(Quote&& q) noexcept : bookNo(std::move(q.bookNo)), price(std::move(q.price))
+    { std::cout << "Quote: move constructor\n";}
+
+    // copy =
+    Quote& operator =(const Quote& rhs)
+    {
+        if (*this != rhs)
+        {
+            bookNo = rhs.bookNo;
+            price = rhs.price;
+        }
+        std::cout << "Quote: copy =()\n";
+
+        return *this;
+    }
+
+    // move =
+    Quote& operator =(Quote&& rhs) noexcept
+    {
+        if (*this != rhs)
+        {
+            bookNo = rhs.bookNo;
+            price = rhs.price;
+        }
+        std::cout << "Quote: move = !!!!\n";
+        return *this;
+    }
 
     std::string isbn() const { return bookNo; }
 
@@ -29,7 +66,10 @@ class Quote{
                   << "price = " << price << " ";
     }
 
-    virtual ~Quote() = default;  // 对析构函数进行动态绑定
+    virtual ~Quote()  // 对析构函数进行动态绑定
+    {
+        std::cout << "destructing Quote\n";
+    }
 
  private:
     std::string bookNo;     // ISBN
@@ -52,9 +92,35 @@ protected:
 
 class Bulk_quote : public Disc_quote {
  public:
-    Bulk_quote() = default;
+    Bulk_quote() { std::cout << "default constructing Bulk_quote\n"; }
     Bulk_quote(const std::string &book, double p, std::size_t qty, double disc) :
-        Disc_quote(book, p, qty, disc) {}
+        Disc_quote(book, p, qty, disc) { std::cout << "Bulk_quote: constructor takes 4 para.\n"; }
+
+    // copy constructor
+    Bulk_quote(const Bulk_quote& nq) : Disc_quote(bq)
+    { std::cout << "Bulk_quote: copy constructor\n"; }
+
+    // move constructor
+    Bulk_quote(Bulk_quote&& bq) noexcept : Disc_quote(std::move(bq))
+    { std::cout << "Bulk_quote: move constructor\n"; }
+
+    // copy =()
+    Bulk_quote& operator =(const Bulk_quote& rhs)
+    {
+        Disc_quote::operator=(rhs);
+        std::cout << "Bulk_quote: copy=() \n";
+
+        return *this;
+    }
+
+    // move=()
+    Bulk_quote& operator =(Bulk_quote&& rhs) noexcept
+    {
+        Disc_quote::operator=(std::move(rhs));
+        std::cout << "Bulk_quote: move=() \n";
+
+        return *this;
+    }
 
     double net_price(std::size_t cnt) const override
     {
@@ -69,6 +135,11 @@ class Bulk_quote : public Disc_quote {
         Quote::debug();
         std::cout << "quantity = " << quantity << " "
                   << "discount = " << discount << " ";
+    }
+
+    ~Bulk_quote() override
+    {
+        std::cout << "destructing Bulk_quote\n";
     }
 
 };
